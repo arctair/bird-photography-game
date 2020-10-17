e038182b924434374edb3b1ed9628efb4d6c61d2
arctair <jtyler_@outlook.com>
2020-10-17 14:13:33 -0500

    refactor

--- main.gd
+++ main.gd
@@ -4,24 +4,23 @@ extends Node2D
 var treeScene = preload('res://Tree.tscn')
 var bluejayScene = preload('res://Bluejay.tscn')
 
+func randomPosition(sprite):
+	var viewportSize = get_viewport().size
+	var spriteSize = sprite.texture.get_size() * sprite.scale / 2
+	return Vector2(
+		rand_range(spriteSize.x, viewportSize.x - spriteSize.x),
+		rand_range(spriteSize.y, viewportSize.y - spriteSize.y)
+	)
+
 func _ready():
 	randomize()
-	var size = get_viewport().size
 	for n in range(16):
 		var treeInstance = treeScene.instance()
-		var treeSize = treeInstance.texture.get_size() * treeInstance.scale / 2
-		treeInstance.position = Vector2(
-			rand_range(treeSize.x, size.x - treeSize.x),
-			rand_range(treeSize.y, size.y - treeSize.y)
-		)
+		treeInstance.position = randomPosition(treeInstance)
 		treeInstance.z_index = treeInstance.position.y
 		add_child(treeInstance)
 	for n in range(4):
 		var birdInstance = bluejayScene.instance()
-		var birdSize = birdInstance.texture.get_size() * birdInstance.scale / 2
-		birdInstance.position = Vector2(
-			rand_range(birdSize.x, size.x - birdSize.x),
-			rand_range(birdSize.y, size.y - birdSize.y)
-		)
+		birdInstance.position = randomPosition(birdInstance)
 		birdInstance.flip_h = randf() > 0.5
 		add_child(birdInstance)
