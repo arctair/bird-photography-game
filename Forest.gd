11e0e0977ac101d4de1972c0cec14e75191d8f35
arctair <jtyler_@outlook.com>
2020-10-18 10:32:12 -0500

    refactor - forest is responsible for spawning trees

---
+++ Forest.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+
+const treeScene := preload('res://Tree.tscn')
+const trees := []
+
+func _ready():
+	for _n in range(16):
+		var tree = treeScene.instance()
+		tree.position = randomPosition(tree)
+		trees.append(tree)
+	trees.sort_custom(PositionYSorter, 'compareByPositionY')
+	for tree in trees:
+		add_child(tree)
+
+func randomPosition(sprite):
+	var viewportSize = get_viewport().size
+	var spriteSize = sprite.texture.get_size() * sprite.scale / 2
+	return Vector2(
+		rand_range(spriteSize.x, viewportSize.x - spriteSize.x),
+		rand_range(spriteSize.y, viewportSize.y - spriteSize.y)
+	)
+
+class PositionYSorter:
+	static func compareByPositionY(s1, s2):
+		return s1.position.y < s2.position.y
